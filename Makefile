435e23bd6333e49b433cba3d6e03b438a0355ef1
lualiliu <lualiliu@outlook.com>
2022-01-07 18:00:05 +0800

    upgrade (1152*1920 DSI to 1280*720 /dev/fb0)
    Add RGA rotation and modify comments. Fix hdmi tearing.

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 FLAGS=`/media/luali/324d947f-8b18-4216-850f-8fe4a7315517/pull/batocera.linux/output/rg552/host/bin/pkg-config --cflags --libs libdrm`
-FLAGS+=-Wall -O0 -g
+FLAGS+=-Wall -O0 -g -lrga
 FLAGS+=-D_FILE_OFFSET_BITS=64
 
 all:
--- drmfbcopy.c
+++ drmfbcopy.c
@@ -1,8 +1,13 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
- * KMS/DRM screenshot tool
+ * RG552 HDMI output tools 
+ * 
+ * The framebuffer of DRM is overwritten to /dev/fb0 through rga. (1152*1920 to 1280*720)
+ * 
+ * Thanks Paul Cercueil <paul@crapouillou.net>
  *
- * Copyright (c) 2021 Paul Cercueil <paul@crapouillou.net>
+ * Author: Luali Liu <lualiliu@outlook.com>
+ *
  */
 
 #include <drm.h>
@@ -19,38 +24,105 @@
 #include <xf86drm.h>
 #include <xf86drmMode.h>
 
-int src_ipr = 1152;
-int dst_ipr = 1920;
+#include <rga/rga.h>
+#include <rga/RgaApi.h>
 
-void rotate(uint16_t *src_p,uint16_t *dst_p,unsigned int len){
-    int row, col;
+static int rga_prepare_info(int bpp, int width, int height, int pitch, rga_info_t *info) {
+    RgaSURF_FORMAT format;
 
-    int src_h = 1920;
-    int src_w = 1152;
-    int dst_h = 1080;
+    memset(info, 0, sizeof(rga_info_t));
 
-    uint16_t* srcBuf;
-    uint16_t* dstBuf;
+    info->fd = -1;
+    info->mmuFlag = 1;
+    info->rotation = HAL_TRANSFORM_ROT_90;
 
-    printf("%d %d",src_ipr,dst_ipr);
-    for (row = 0; row < src_h; ++row)
+    switch (bpp) {
+    case 12:
+        format = RK_FORMAT_YCbCr_420_SP;
+        break;
+    case 16:
+        format = RK_FORMAT_RGB_565;
+        break;
+    case 32:
+        format = RK_FORMAT_BGRA_8888;
+        break;
+    default:
+        return -1;
+    }
+
+    rga_set_rect(&info->rect, 0, 0, width, height,
+                 pitch * 8 / bpp, height, format);
+    return 0;
+}
+
+static int drm_render_rga(void *buf, void *dst_buf, int bpp, int width, int height, int dst_width, int dst_height, int pitch, int dst_pitch) {
+    rga_info_t src_info = {0};
+    rga_info_t dst_info = {0};
+
+    static int rga_supported = 1;
+    static int rga_inited = 0;
+
+    if (!rga_supported)
+        return -1;
+
+    if (!rga_inited) {
+        if (c_RkRgaInit() < 0) {
+            rga_supported = 0;
+            return -1;
+        }
+        rga_inited = 1;
+    }
+
+    if (rga_prepare_info(bpp, width, height, pitch, &src_info) < 0)
+        return -1;
+
+    if (rga_prepare_info(bpp, dst_width, dst_height, dst_pitch, &dst_info) < 0)
+        return -1;
+
+    src_info.virAddr = buf;
+    dst_info.virAddr = dst_buf;
+
+    return c_RkRgaBlit(&src_info, &dst_info, NULL);
+}
+
+static void rotate_90_ccw(uint32_t* restrict dst, uint32_t* restrict src, int32_t w, int32_t h)
+{
+    src += w * h - 1;
+    for (int32_t col = w - 1; col >= 0; --col)
     {
-        srcBuf = (uint16_t*)(src_p) + (row * src_ipr);
-        dstBuf = (uint16_t*)(dst_p) + row + ((dst_h - 1) * dst_ipr);
-        for (col = 0; col < src_w; ++col)
+        uint32_t *outcol = dst + col;
+        for(int32_t row = 0; row < h; ++row, outcol += w)
+        {
+            *outcol = *src--;
+        }
+    }
+}
+
+/*
+static void rotate_90(uint32_t* restrict dst, uint32_t* restrict src, int32_t w, int32_t h)
+{
+
+    for (int32_t row = 0; row < h; ++row)
+    {
+        uint32_t* srcBuf = (uint32_t *)(src) + (row * w);
+        uint32_t* dstBuf = (uint32_t *)(dst) + row + ((h - 1) * h);
+        for (int32_t col = 0; col < w; ++col)
         {
             *dstBuf = *srcBuf;
             ++srcBuf;
-            dstBuf -= dst_ipr;
+            dstBuf -= w;
         } 
     }
 }
+*/
 
 static int save_fb(drmModeFB *fb, int prime_fd)
 {
 	void *buffer, *picture;
 	int ret = 0;
 	unsigned int len = (fb->bpp >> 3) * fb->width * fb->height;
+
+	unsigned int len_720p = (fb->bpp >> 3) * 1280 * 720;
 
 	/* open screen file. */
 	int fb0_fd = open("/dev/fb0",O_RDWR);
@@ -72,11 +144,15 @@ static int save_fb(drmModeFB *fb, int prime_fd)
 	/* Drop privileges, to write PNG with user rights */
 	seteuid(getuid());
 
-	// Not implemented
-	//rotate(picture, buffer, len);
+	void *membuffer = malloc(len_720p);
 
-	memcpy(picture,buffer,len);
-
+	// Rotation and zoom out
+	while(1){
+	//	rotate_90_ccw(picture, membuffer, 1920,1152);
+		ret = drm_render_rga(buffer, membuffer, 32,1152,1920,1280,720,1152*4,1280*4);
+		memcpy(picture,membuffer,len_720p);
+		usleep(3);	//The RGA actual time-consuming of 1280*720 is 2,000us
+	}
 	return ret;
 }
 
@@ -90,6 +166,9 @@ int main(int argc, char **argv)
 	drmModeFB *fb;
 	char buf[256];
 	uint64_t has_dumb;
+
+	//create daemon
+	daemon(0,0);
 
 	for (card = 0; ; card++) {
 		snprintf(buf, sizeof(buf), "/dev/dri/card%u", card);
